31a97f16962cb90b0c1dd2efaeb6e550f1d1366c
unknown <larserikgk@gmail.com>
2013-04-08 19:45:10 +0200

    Superalphadev on view and html for userprofile

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from datetime import datetime
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect, get_object_or_404
+
+from caglols.userprofile.models import UserProfile
+
+@login_required
+def my_profile(request):
+    profile = request.user.get_profile()
+
+    return render(request, 'userprofile/profile.html', {'quser': request.user, 'profile': profile})	
